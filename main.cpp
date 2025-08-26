f6ceddc2e3dcad07d473b5ae36752a44356f6ff5
zemarinho <zemiguelmarinho05@hotmail.com>
2025-08-26 21:51:15 +0100

    Recursivo mais rápido

--- main.cpp
+++ main.cpp
@@ -17,16 +17,15 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> temp, auxv;
-        if (root != nullptr){
-            if (root->left != nullptr){
-                auxv = inorderTraversal(root->left);
-                temp.insert(temp.end(), auxv.begin(), auxv.end());
-            }
-            temp.push_back(root->val);
-            if (root->right != nullptr){
-                auxv = inorderTraversal(root->right);
-                temp.insert(temp.end(), auxv.begin(), auxv.end());
-            }
+        if (root == nullptr){return temp;}
+        if (root->left != nullptr){
+            auxv = inorderTraversal(root->left);
+            temp.insert(temp.end(), auxv.begin(), auxv.end());
+        }
+        temp.push_back(root->val);
+        if (root->right != nullptr){
+            auxv = inorderTraversal(root->right);
+            temp.insert(temp.end(), auxv.begin(), auxv.end());
         }
         return temp;
     }
